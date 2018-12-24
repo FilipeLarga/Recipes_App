cf145db10e732eaa5c787bc1a72d664d82d242c4
Filipe Larga <fesvl@iscte-iul.pt>
2018-12-24 00:50:19 +0000

    View Recipes is looking good, considering swapping the images

--- app/src/main/java/figuitosinc/pocketchef/CategoriesEnum.java
+++ app/src/main/java/figuitosinc/pocketchef/CategoriesEnum.java
@@ -6,7 +6,6 @@ public class CategoriesEnum {
 
     public final static String MEAT = "Meat";
     public final static String FISH = "Fish";
-    public final static int count = 8;
     private final static String ASIAN = "Asian";
     private final static String BREAKFAST = "Breakfast";
     private final static String ITALIAN = "Italian";
@@ -15,7 +14,14 @@ public class CategoriesEnum {
     private final static String SALAD = "Salad";
     private final static String SEAFOOD = "Seafood";
     private final static String VEGETARIAN = "Vegetarian";
-    public final static String[] categories = new String[]{/*FAVOURITE, MEAT, FISH*/ ASIAN, BREAKFAST, ITALIAN, MEXICAN, PIZZA, SALAD, SEAFOOD, VEGETARIAN, "Test1", "Test2", "Tesr3", "Test4", "Test 5",};
+
+    public final static int count = 11;
+    private final static String Test1 = "Test1";
+    private final static String Test2 = "Test2";
+    private final static String Test3 = "Test3";
+    private final static String Test4 = "Test4";
+    private final static String Test5 = "Test5";
+    public final static String[] categories = new String[]{/*FAVOURITE, MEAT, FISH*/ ASIAN, BREAKFAST, ITALIAN, MEXICAN, PIZZA, SALAD, SEAFOOD, VEGETARIAN, Test1, Test2, Test3, Test4, Test5};
 
 
 }
